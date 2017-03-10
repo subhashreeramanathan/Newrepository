ca6717af18804df8a1c4abf29425434843bc81eb
ROOT\subhashree.ramanathan <subhashreeramanathan@syncfusion.com>
2017-03-10 14:32:34 +0530

    commit

--- app.component.ts
+++ app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'my-app',
-    template:`<div><h1>Title</h1></div>`
+    template:`<div><h1>{{Title}}</h1></div>`
 })
-export class AppComponent {}
+export class AppComponent {
+    Title : string ="Hello Angular2";
+}
